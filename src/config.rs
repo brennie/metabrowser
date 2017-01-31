7e7a304d8c63c9e4b98564d0a7efdd875723e480
Barret Rennie <barret@brennie.ca>
2017-01-30 23:52:50 -0600

    Switch to error-chain over error-type
    error-chain is a more widely-used & supported library for handling
    errors and unifying multiple error types.

--- src/config.rs
+++ src/config.rs
@@ -1,23 +1,21 @@
 use std::collections::HashMap;
 use std::fs::File;
-use std::io;
 use std::io::prelude::*;
 use std::path::Path;
-use std::result;
 
 use serde_yaml;
 
 
-error_type! {
-    #[derive(Debug)]
-    pub enum Error {
-        GenericError(String) {
-            desc (e) &**e;
-        },
-        IOError(io::Error) { cause; },
-        ParseError(serde_yaml::Error) {
-            cause;
-        },
+error_chain! {
+    foreign_links {
+        ParseError(::serde_yaml::Error);
+        IOError(::std::io::Error);
+    }
+
+    errors {
+        NoHomeError {
+            description("could not locate home directory")
+        }
     }
 }
 
@@ -45,9 +43,8 @@ pub struct Config {
 }
 
 
-pub type Result = result::Result<Config, Error>;
 impl Config {
-    pub fn from_file(p: &Path) -> Result {
+    pub fn from_file(p: &Path) -> Result<Config> {
         let mut f = try!(File::open(p));
         let mut buf = String::new();
 
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #[macro_use]
-extern crate error_type;
+extern crate error_chain;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_yaml;
 
 mod config;
 
-use self::config::{Config, Error};
+use self::config::{Config, Error, ErrorKind};
 use std::path::Path;
 use std::io::{stderr, Write};
 use std::env::home_dir;
@@ -14,7 +14,7 @@ use std::env::home_dir;
 
 fn main() {
     let result = home_dir()
-        .ok_or(Error::GenericError(String::from("could not locate home directory.")))
+        .ok_or(Error::from(ErrorKind::NoHomeError))
         .map(|p| Path::new(&p).join(".metabrowser.yaml"))
         .and_then(|p| Config::from_file(&p));
 
